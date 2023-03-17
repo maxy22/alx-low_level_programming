b4cc99bd85be347e01a03bda5444a32536cb6335
maxy22 <mdotsey17@gmail.com>
2023-03-16 21:08:13 -0700

    6-size.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Using the puts function to print.
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+Return (0);
+}
