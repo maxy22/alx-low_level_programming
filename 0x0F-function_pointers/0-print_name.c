141bca25b2f0e3c2d3458753ad8ee48709b1d82c
maxy22 <mdotsey17@gmail.com>
2023-04-20 16:44:48 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -17,12 +17,11 @@
 void print_name(char *name, void (*f)(char *))
 
 {
+	if (name == NULL || f == NULL) {
+   printf("Error: NULL parameter passed.\n");
+   return;
+}
 
-	        if (name == NULL || f == NULL)
-
-		                return;
-
-	        f(name);
 
 }
 
